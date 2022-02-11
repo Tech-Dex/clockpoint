a6b9a2b4465626f96ea66a01e1eb52af43256baf
Tech-Dex <rominu.alexandru@yahoo.com>
2022-02-11 22:24:03 +0200

    Add permissions table

---
+++ server/app/alembic/versions/2022-02-11_bcd49b2b8794_create_permissions_table.py
@@ -0,0 +1,64 @@
+"""create permissions table
+
+Revision ID: bcd49b2b8794
+Revises: d43649786045
+Create Date: 2022-02-11 22:08:26.111644
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "bcd49b2b8794"
+down_revision = "d43649786045"
+branch_labels = None
+depends_on = None
+
+# "ix": "ix_%(column_0_label)s"
+# "uq": "uq_%(table_name)s_%(column_0_name)s"
+# "ck": "ck_%(table_name)s_%(constraint_name)s"
+# "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
+# "pk": "pk_%(table_name)s"
+
+
+def upgrade():
+    roles_table = op.create_table(
+        "permissions",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("permission", sa.String(255), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime,
+            server_default=sa.func.current_timestamp(),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime,
+            server_default=sa.func.current_timestamp(),
+            nullable=False,
+        ),
+        sa.Column("deleted_at", sa.DateTime, nullable=True),
+    )
+
+    op.create_index("ix_permission", "permissions", ["permission"], unique=True)
+
+    op.bulk_insert(
+        roles_table,
+        [
+            {"permission": "view_own_report"},
+            {"permission": "invite_user"},
+            {"permission": "kick_user"},
+            {"permission": "generate_report"},
+            {"permission": "view_report"},
+            {"permission": "edit"},
+            {"permission": "delete"},
+            {"permission": "add_admin"},
+            {"permission": "remove_admin"},
+        ],
+    )
+
+
+def downgrade():
+    op.drop_table("permissions")
---
+++ server/app/alembic/versions/2022-02-11_deee135305c9_create_roles_permissions_table.py
@@ -0,0 +1,94 @@
+"""create roles_permissions table
+
+Revision ID: deee135305c9
+Revises: bcd49b2b8794
+Create Date: 2022-02-11 22:13:28.476123
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "deee135305c9"
+down_revision = "bcd49b2b8794"
+branch_labels = None
+depends_on = None
+
+# "ix": "ix_%(column_0_label)s"
+# "uq": "uq_%(table_name)s_%(column_0_name)s"
+# "ck": "ck_%(table_name)s_%(constraint_name)s"
+# "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
+# "pk": "pk_%(table_name)s"
+
+
+def upgrade():
+    roles_permissions_table = op.create_table(
+        "roles_permissions",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("roles_id", sa.Integer, nullable=False),
+        sa.Column("permissions_id", sa.Integer, nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime,
+            server_default=sa.func.current_timestamp(),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime,
+            server_default=sa.func.current_timestamp(),
+            nullable=False,
+        ),
+        sa.Column("deleted_at", sa.DateTime, nullable=True),
+    )
+
+    op.create_foreign_key(
+        "fk_roles_permissions_roles_id_roles",
+        "roles_permissions",
+        "roles",
+        ["roles_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+    op.create_foreign_key(
+        "fk_roles_permissions_permissions_id_roles",
+        "roles_permissions",
+        "permissions",
+        ["permissions_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+    op.create_index(
+        "ix_groups_users_roles_id_permissions_id",
+        "roles_permissions",
+        ["roles_id", "permissions_id"],
+        unique=True,
+    )
+
+    op.bulk_insert(
+        roles_permissions_table,
+        [
+            {"roles_id": 1, "permissions_id": 1},
+            {"roles_id": 1, "permissions_id": 2},
+            {"roles_id": 1, "permissions_id": 3},
+            {"roles_id": 1, "permissions_id": 4},
+            {"roles_id": 1, "permissions_id": 5},
+            {"roles_id": 1, "permissions_id": 6},
+            {"roles_id": 1, "permissions_id": 7},
+            {"roles_id": 1, "permissions_id": 8},
+            {"roles_id": 1, "permissions_id": 9},
+            {"roles_id": 2, "permissions_id": 1},
+            {"roles_id": 2, "permissions_id": 2},
+            {"roles_id": 2, "permissions_id": 3},
+            {"roles_id": 2, "permissions_id": 4},
+            {"roles_id": 2, "permissions_id": 5},
+            {"roles_id": 3, "permissions_id": 1},
+        ],
+    )
+
+
+def downgrade():
+    op.drop_table("roles_permissions")

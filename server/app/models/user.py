1df0c8eba2a3579fd4c4c35a10504ebbd721fb53
Tech-Dex <rominu.alexandru@yahoo.com>
2022-02-11 21:56:45 +0200

    Add user where deleted_at is Null

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -36,7 +36,7 @@ class DBUser(DBCoreModel, BaseUser):
     @classmethod
     async def get_by_id(cls, mysql_driver: Database, user_id: int) -> "DBUser":
         users: Table = Table("users")
-        query = MySQLQuery.from_(users).select("*").where(users.id == Parameter(":id"))
+        query = MySQLQuery.from_(users).select("*").where(users.id == Parameter(":id")).where(users.is_deleted.isnull())
         values = {"id": user_id}
 
         user: Mapping = await mysql_driver.fetch_one(
@@ -53,7 +53,7 @@ class DBUser(DBCoreModel, BaseUser):
         query = (
             MySQLQuery.from_(users)
             .select("*")
-            .where(users.email == Parameter(":email"))
+            .where(users.email == Parameter(":email")).where(users.is_deleted.isnull())
         )
         values = {"email": email}
 
